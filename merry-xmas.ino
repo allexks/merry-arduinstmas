a18ad206cbaadec086db3af3bdc4dc24cc030350
Yalishanda <ani1802@abv.bg>
2018-12-04 16:54:34 +0200

    refactor code

--- merry-xmas.ino
+++ merry-xmas.ino
@@ -11,12 +11,14 @@ Adafruit_SSD1306 display(OLED_RESET);
 #define YPOS 1
 #define DELTAY 2
 
-#define LOGO16_GLCD_HEIGHT 16 
+#define LOGO16_GLCD_HEIGHT 16
 #define LOGO16_GLCD_WIDTH  32
 
 uint8_t icons[NUMFLAKES][3];
 
-static const unsigned char PROGMEM logo16_glcd_bmp[] =
+void initializeIcon(uint8_t*);
+
+static const unsigned char PROGMEM LOGO16_GLCD_BMP[] =
 { B00100000, B00001000, B00100000, B00001000,
   B10100000, B10000010, B10000010, B00001010,
   B00001000, B00100010, B10001000, B00100000,
@@ -52,16 +54,14 @@ void setup() {
     pinMode(PINS[i], OUTPUT);
     digitalWrite(PINS[i], HIGH);
   }
-  
+
   display.display();
   delay(2000);
   display.clearDisplay();
- 
-  for (uint8_t f=0; f< NUMFLAKES; f++) {
-    icons[f][XPOS] = random(display.width());
-    icons[f][YPOS] = 0;
-    icons[f][DELTAY] = random(5) + 1;
-    
+
+  for (uint8_t f=0; f < NUMFLAKES; f++) {
+    initializeIcon(icons[f]);
+
     Serial.print("x: ");
     Serial.print(icons[f][XPOS], DEC);
     Serial.print(" y: ");
@@ -70,32 +70,36 @@ void setup() {
     Serial.println(icons[f][DELTAY], DEC);
   }
 
-  
+
 }
 
 void loop() {
     int w = LOGO16_GLCD_WIDTH;
     int h = LOGO16_GLCD_HEIGHT;
-    
+
     // draw each icon
-    for (uint8_t f=0; f< NUMFLAKES; f++) {
-      display.drawBitmap(icons[f][XPOS], icons[f][YPOS], logo16_glcd_bmp, w, h, WHITE);
+    for (uint8_t f=0; f < NUMFLAKES; f++) {
+      display.drawBitmap(icons[f][XPOS], icons[f][YPOS], LOGO16_GLCD_BMP, w, h, WHITE);
     }
     display.display();
     delay(200);
-    
+
     // then erase it + move it
-    for (uint8_t f=0; f< NUMFLAKES; f++) {
-      display.drawBitmap(icons[f][XPOS], icons[f][YPOS],  logo16_glcd_bmp, w, h, BLACK);
-      
+    for (uint8_t f=0; f < NUMFLAKES; f++) {
+      display.drawBitmap(icons[f][XPOS], icons[f][YPOS],  LOGO16_GLCD_BMP, w, h, BLACK);
+
       // move it
       icons[f][YPOS] += icons[f][DELTAY];
-      
+
       // if its gone, reinit
       if (icons[f][YPOS] > display.height()) {
-        icons[f][XPOS] = random(display.width());
-        icons[f][YPOS] = 0;
-        icons[f][DELTAY] = random(5) + 1;
+        initializeIcon(icons[f]);
       }
     }
+}
+
+void initializeIcon(uint8_t* icon) {
+    icon[XPOS] = random(display.width());
+    icon[YPOS] = 0;
+    icon[DELTAY] = random(5) + 1;
 }
