dd52f91c03409013ae41fd81f5c0c4c7eeab1b14
Yalishanda <ani1802@abv.bg>
2018-12-05 12:27:05 +0200

    refactor code for easier funcion addition

--- merry-xmas.ino
+++ merry-xmas.ino
@@ -17,6 +17,8 @@ Adafruit_SSD1306 display(OLED_RESET);
 uint8_t icons[NUMFLAKES][3];
 
 void initializeIcon(uint8_t*);
+void drawIcons();
+void eraseAndMoveIcons();
 
 static const unsigned char PROGMEM LOGO16_GLCD_BMP[] =
 { B00100000, B00001000, B00100000, B00001000,
@@ -44,21 +46,44 @@ static const unsigned char PROGMEM LOGO16_GLCD_BMP[] =
 
 const int PINS[NUM_PINS] = {WHITE_PIN, GREEN_PIN, ORANGE_PIN, YELLOW_PIN};
 
+enum modes {
+  ALL_ON,
+  ALL_BLINK_CONSECUTIVELY,
+  ALL_BLINK_TOGETHER,
+  __COUNT__ // do not remove; always put at end
+};
+
+const modes PROGRAM[] = {
+    ALL_BLINK_CONSECUTIVELY,
+    ALL_BLINK_TOGETHER,
+    ALL_ON
+};
+
+uint8_t programIndex = 0;
+
+#define MODE_UPDATE_INTV 8000   // ms
+#define DISPLAY_UPDATE_INTV 200 // ms
+
+long prevMillisMode = 0;
+long prevMillisDisplay = 0;
+long currMillisMode = 0;
+long currMillisDisplay = 0;
+
 
 void setup() {
   Serial.begin(9600);
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C);  // initialize with the I2C addr 0x3C (for the 128x32)
 
+  // splash screen
   for (int i=0; i < NUM_PINS; i++) {
-    // turn on each LED
     pinMode(PINS[i], OUTPUT);
     digitalWrite(PINS[i], HIGH);
   }
-
   display.display();
   delay(2000);
   display.clearDisplay();
 
+  // initialize all icons
   for (uint8_t f=0; f < NUMFLAKES; f++) {
     initializeIcon(icons[f]);
 
@@ -70,23 +95,39 @@ void setup() {
     Serial.println(icons[f][DELTAY], DEC);
   }
 
+  // initial draw
+  drawIcons();
 
+  prevMillisMode = millis();
+  currMillisMode = millis();
+  prevMillisDisplay = millis();
+  currMillisDisplay = millis();
 }
 
-void loop() {
-    int w = LOGO16_GLCD_WIDTH;
-    int h = LOGO16_GLCD_HEIGHT;
 
-    // draw each icon
+void loop() {
+    drawIcons();
+    delay(200);
+    eraseAndMoveIcons();
+}
+
+
+void initializeIcon(uint8_t* icon) {
+    icon[XPOS] = random(display.width());
+    icon[YPOS] = 0;
+    icon[DELTAY] = random(5) + 1;
+}
+
+void drawIcons() {
     for (uint8_t f=0; f < NUMFLAKES; f++) {
-      display.drawBitmap(icons[f][XPOS], icons[f][YPOS], LOGO16_GLCD_BMP, w, h, WHITE);
+      display.drawBitmap(icons[f][XPOS], icons[f][YPOS], LOGO16_GLCD_BMP, LOGO16_GLCD_WIDTH, LOGO16_GLCD_HEIGHT, WHITE);
     }
     display.display();
-    delay(200);
+}
 
-    // then erase it + move it
+void eraseAndMoveIcons() {
     for (uint8_t f=0; f < NUMFLAKES; f++) {
-      display.drawBitmap(icons[f][XPOS], icons[f][YPOS],  LOGO16_GLCD_BMP, w, h, BLACK);
+      display.drawBitmap(icons[f][XPOS], icons[f][YPOS],  LOGO16_GLCD_BMP, LOGO16_GLCD_WIDTH, LOGO16_GLCD_HEIGHT, BLACK);
 
       // move it
       icons[f][YPOS] += icons[f][DELTAY];
@@ -96,10 +137,4 @@ void loop() {
         initializeIcon(icons[f]);
       }
     }
-}
-
-void initializeIcon(uint8_t* icon) {
-    icon[XPOS] = random(display.width());
-    icon[YPOS] = 0;
-    icon[DELTAY] = random(5) + 1;
 }
